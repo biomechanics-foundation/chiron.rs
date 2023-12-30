217eaf4e0452270a1d15b5b1a62eba08d2ce4488
Claire V. Hammond <cvhammond@mailbox.org>
2023-12-30 01:07:13 -0800

    initial commit

---
+++ docs/intro.md
@@ -0,0 +1,40 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Let's run through **Chiron in less than 5 minutes**.
+
+## Getting Started
+
+Get started by downloading Chiron. You can do this through `Cargo`, the package manager for Rust.
+
+First, visit [rustup](https://rustup.rs/) to install Rust.
+
+Second, run the following command:
+
+```bash
+cargo install chiron
+```
+
+## Using Chiron
+
+From here, you can use any of the programs from Chiron simply by typing their name in the command window.
+
+For example:
+
+```bash
+c3dio
+```
+
+## Learning to use Chiron
+
+Chiron consists of a number of programs to faciliate biomechanics. These include:
+
+- camera-based markered motion capture (TBA)
+- data analysis (TBA)
+- C3D reading, writing, and processing (c3dio)
+- Inverse kinematics and inverse dynamics (TBA)
+
+Read on to learn about these projects.
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <img src="/img/logo.png" alt="Chiron Logo" width="200" height="200" />
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Build with ${siteConfig.title}`}
+      description="Chiron is an open-source biomechanics software project written in Rust.">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
